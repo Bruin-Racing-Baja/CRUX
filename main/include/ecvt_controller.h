135890051b4ca28417161b68070a8c3d68a568df
sarah darzacq <sarahdarzacq22@gmail.com>
2026-04-15 18:33:33 -0700

    cleanup ecvt controller class

--- main/include/ecvt_controller.h
+++ main/include/ecvt_controller.h
@@ -11,18 +11,16 @@
 #include <input_output/shift_register.h>
 #include <odrive.h> 
 
-class ECVTController {
+class ECVTController 
+{
 public:
     ECVTController(ShiftRegister* sr, bool wait_for_can = true);
-
     void init(bool wait_for_can=true, Telemetry* telem = nullptr);
-    bool home_actuator(uint32_t timeout_ms=5000); 
-    
-    void control_function(); 
-    void button_shift_control_function(); 
-    void normal_control_function(); 
 
 private:
+    bool home_actuator(uint32_t timeout_ms=5000);
+    void control_loop();
+
     bool get_outbound_limit(); 
     bool get_inbound_limit(); 
     bool get_engage_limit();
@@ -32,27 +30,25 @@ private:
     static IRAM_ATTR void outbound_isr(void* p = nullptr);
     static IRAM_ATTR void inbound_isr(void* p = nullptr);
 
+    static void timerCallback(void* arg);
+    static void taskWrapper(void* pvParameters);
+
+    static ECVTController* instance;
+
     GearToothSensor primary_gts; 
     GearToothSensor secondary_gts; 
 
     ODrive odrive;
-
     ShiftRegister* shift_reg;
-    
     Telemetry* telem;
 
     uint64_t control_cycle_count;
+    float last_engine_rpm_error;
+    float actuator_engage_position;
 
     TaskHandle_t taskHandle;
     esp_timer_handle_t timerHandle;
-
-    static ECVTController* instance;
-
-    void control_loop();
-    static void timerCallback(void* arg);
-    static void taskWrapper(void* pvParameters);
    
-
     IIRFilter engine_rpm_rotation_filter{
         ENGINE_RPM_ROTATION_FILTER_B, ENGINE_RPM_ROTATION_FILTER_A,
         ENGINE_RPM_ROTATION_FILTER_M, ENGINE_RPM_ROTATION_FILTER_N
@@ -79,9 +75,6 @@ private:
     };
 
     MedianFilter engine_rpm_median_filter{ENGINE_RPM_MEDIAN_FILTER_WINDOW};
-    float last_engine_rpm_error;
-
-    float actuator_engage_position;
 };
 
 #endif // ECVT_CONTROLLER_H
--- main/src/ecvt_controller.cpp
+++ main/src/ecvt_controller.cpp
@@ -12,6 +12,7 @@ ECVTController::ECVTController(ShiftRegister* sr, bool wait_for_can)
       odrive(ECVT_ODRIVE_NODE_ID),
       shift_reg(sr), 
       control_cycle_count(0),
+      last_engine_rpm_error(0),
       actuator_engage_position(0)
 {
     instance = this; 
@@ -23,16 +24,15 @@ void ECVTController::init(bool wait_for_can, Telemetry* telem)
         ESP_LOGE(TAG, "Instance not set");
         return;
     }
+
+    /* Initialize limit switch pins */
     pinMode(ECVT_LIMIT_SWITCH_INBOUND_PIN, PinMode::INPUT_ONLY);
     pinMode(ECVT_LIMIT_SWITCH_OUTBOUND_PIN, PinMode::INPUT_ONLY);
 
+    /* Initialize CAN BUS */
     odrive.init(CAN_TX_PIN, CAN_RX_PIN, CAN_BITRATE);
     odrive.start();
     odrive.clear_errors();
-
-    shift_reg->write_all_leds(true);
-    vTaskDelay(pdMS_TO_TICKS(2000));
-    shift_reg->write_all_leds(false);
 
     /* Wait for CAN Heartbeat - Blinking LEDs */
     if (wait_for_can) {
@@ -44,19 +44,22 @@ void ECVTController::init(bool wait_for_can, Telemetry* telem)
             state = !state;
         }
     }
-    shift_reg->write_all_leds(false);
+    shift_reg->write_all_leds(true);
 
     bool homed = home_actuator(); 
     if (homed) {
         ESP_LOGI(TAG, "Actuator Homed!");
+        shift_reg->write_all_leds(false);
     }
 
+    /* Initialize Interrupts */
     attachInterrupt(ENGINE_GEARTOOTH_SENSOR_PIN, primary_isr, InterruptMode::RISING_EDGE);    
     attachInterrupt(GEARBOX_GEARTOOTH_SENSOR_PIN, secondary_isr, InterruptMode::RISING_EDGE);
 
     attachInterrupt(ECVT_LIMIT_SWITCH_INBOUND_PIN, inbound_isr, InterruptMode::RISING_EDGE);
     attachInterrupt(ECVT_LIMIT_SWITCH_OUTBOUND_PIN, outbound_isr, InterruptMode::RISING_EDGE);
     
+    /* Begin Control Loop as Async Task */
     xTaskCreatePinnedToCore(taskWrapper, "ecvt_task", 4096, this, 10, &taskHandle, 1);
 
     const esp_timer_create_args_t timer_args = {
@@ -64,96 +67,16 @@ void ECVTController::init(bool wait_for_can, Telemetry* telem)
         .arg = this,
         .name = "ecvt_timer"
     };
+
     esp_timer_create(&timer_args, &timerHandle);
     esp_timer_start_periodic(timerHandle, 10000);
 }
 
-void ECVTController::timerCallback(void* arg) {
-    ECVTController* controller = (ECVTController*)arg;
-    vTaskNotifyGiveFromISR(controller->taskHandle, NULL);
-}
-void ECVTController::taskWrapper(void* pvParameters) {
-    ((ECVTController*)pvParameters)->control_loop();
-}
-void ECVTController::control_loop()
-{
-    float dt_s = CONTROL_FUNCTION_INTERVAL_MS * SECONDS_PER_MS;
-    float override = 0.0f;
-    
-    while(true)
-    {
-        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-
-        float primary_rpm = primary_gts.get_rpm();
-        float secondary_rpm = secondary_gts.get_rpm();
-        
-        float filtered_primary_rpm = engine_rpm_median_filter.update(primary_rpm);
-        filtered_primary_rpm = engine_rpm_time_filter.update(filtered_primary_rpm);
-
-        float filtered_secondary_rpm = gear_rpm_time_filter.update(secondary_rpm);
-        filtered_secondary_rpm = filtered_secondary_rpm / GEAR_TO_SECONDARY_RATIO;
-
-        float engine_rpm_error = filtered_primary_rpm - ECVT_TARGET_RPM;
-        float filtered_engine_rpm_error = engine_rpm_derror_filter.update(engine_rpm_error);
-
-        float engine_rpm_derror =
-            (filtered_engine_rpm_error - last_engine_rpm_error) / dt_s;
-        last_engine_rpm_error = filtered_engine_rpm_error;
-
-        
-        float velocity_command =
-            (engine_rpm_error * ACTUATOR_KP +
-            MAX(0, engine_rpm_derror * ACTUATOR_KD));
-        velocity_command = CLAMP(velocity_command, -VELOCITY_LIMIT, VELOCITY_LIMIT);
-        odrive.set_axis_state(AXIS_STATE_CLOSED_LOOP_CONTROL);
-        
-        odrive.set_controller_mode(CTRL_MODE_VELOCITY_CONTROL, INPUT_MODE_PASSTHROUGH);
-
-        if (odrive.get_pos() * ECVT_DIR <= actuator_engage_position + 0.1 && velocity_command <= 0) {
-            if(odrive.get_pos() * ECVT_DIR > actuator_engage_position - 0.1)
-                odrive.set_axis_state(AXIS_STATE_IDLE);
-            else
-            {
-                odrive.set_controller_mode(CTRL_MODE_POSITION_CONTROL, INPUT_MODE_PASSTHROUGH);
-                odrive.set_input_pos(actuator_engage_position * ECVT_DIR, 0, 0);
-            }
-            shift_reg->write_led(3, true);
-
-        }
-        else{
-            odrive.set_axis_state(AXIS_STATE_CLOSED_LOOP_CONTROL);
-            odrive.set_controller_mode(CTRL_MODE_VELOCITY_CONTROL, INPUT_MODE_PASSTHROUGH);
-            odrive.set_input_vel(velocity_command * ECVT_DIR, 0.0f);
-        }
-
-        uint64_t time_us = esp_timer_get_time();
-        Telemetry::back_buffer->time_ms = (float) time_us / 1e3;
-
-        Telemetry::back_buffer->engine_rpm = primary_rpm;
-        Telemetry::back_buffer->secondary_rpm = secondary_rpm; 
-
-        Telemetry::back_buffer->filtered_engine_rpm = filtered_primary_rpm;
-        Telemetry::back_buffer->filtered_secondary_rpm = filtered_secondary_rpm;
-
-        Telemetry::back_buffer->target_rpm = ECVT_TARGET_RPM;
-        Telemetry::back_buffer->engine_rpm_error = engine_rpm_error; 
-
-        Telemetry::back_buffer->velocity_command = velocity_command; 
-        
-        Telemetry::back_buffer->ecvt_velocity = odrive.get_vel() * ECVT_DIR;
-        Telemetry::back_buffer->ecvt_pos = odrive.get_pos() * ECVT_DIR;
-        Telemetry::back_buffer->ecvt_iq = odrive.get_iq();
-
-        Telemetry::back_buffer->inbound_limit_switch = get_inbound_limit(); 
-        Telemetry::back_buffer->outbound_limit_switch = get_outbound_limit(); 
-        Telemetry::back_buffer->engage_limit_switch = get_engage_limit(); 
-        
-        Telemetry::back_buffer = Telemetry::front_buffer.exchange(Telemetry::back_buffer);
-
-        control_cycle_count++;
-    }
-}
-
+/**
+ * Run shift fork through homing sequence
+ * 1. Shift out to outbound limit switch 
+ * 2. Shift in to engaged limit switch 
+*/
 bool ECVTController::home_actuator(uint32_t timeout_ms) 
 {
     /* Add delay to give ODrive time to initialize */
@@ -192,6 +115,94 @@ bool ECVTController::home_actuator(uint32_t timeout_ms)
     return true; 
 }
 
+/**
+ * Set actuator velocity based on PID controller.
+ * Called through pinned task. 
+*/
+void ECVTController::control_loop()
+{
+    float dt_s = CONTROL_FUNCTION_INTERVAL_MS * SECONDS_PER_MS;
+    float override = 0.0f;
+    
+    while(true)
+    {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        /* Grab sensor data */
+        float primary_rpm = primary_gts.get_rpm();
+        float secondary_rpm = secondary_gts.get_rpm();
+        
+        /* Filter RPMs */
+        float filtered_primary_rpm = engine_rpm_median_filter.update(primary_rpm);
+        filtered_primary_rpm = engine_rpm_time_filter.update(filtered_primary_rpm);
+
+        float filtered_secondary_rpm = gear_rpm_time_filter.update(secondary_rpm);
+        filtered_secondary_rpm = filtered_secondary_rpm / GEAR_TO_SECONDARY_RATIO;
+
+        /* RPM error from target */
+        float engine_rpm_error = filtered_primary_rpm - ECVT_TARGET_RPM;
+        float filtered_engine_rpm_error = engine_rpm_derror_filter.update(engine_rpm_error);
+
+        float engine_rpm_derror =
+            (filtered_engine_rpm_error - last_engine_rpm_error) / dt_s;
+        last_engine_rpm_error = filtered_engine_rpm_error;
+
+        /* Calculate velocity command */
+        float velocity_command =
+            (engine_rpm_error * ACTUATOR_KP +
+            MAX(0, engine_rpm_derror * ACTUATOR_KD));
+        velocity_command = CLAMP(velocity_command, -VELOCITY_LIMIT, VELOCITY_LIMIT);
+        
+        odrive.set_axis_state(AXIS_STATE_CLOSED_LOOP_CONTROL);
+        odrive.set_controller_mode(CTRL_MODE_VELOCITY_CONTROL, INPUT_MODE_PASSTHROUGH);
+
+        /** Don't allow full shift out, keep shift fork at engaged limit switch when shifting out. */
+        if (odrive.get_pos() * ECVT_DIR <= actuator_engage_position + 0.1 && velocity_command <= 0) {
+            if(odrive.get_pos() * ECVT_DIR > actuator_engage_position - 0.1)
+                odrive.set_axis_state(AXIS_STATE_IDLE);
+            else
+            {
+                odrive.set_controller_mode(CTRL_MODE_POSITION_CONTROL, INPUT_MODE_PASSTHROUGH);
+                odrive.set_input_pos(actuator_engage_position * ECVT_DIR, 0, 0);
+            }
+            shift_reg->write_led(3, true);
+
+        }
+        else{
+            odrive.set_axis_state(AXIS_STATE_CLOSED_LOOP_CONTROL);
+            odrive.set_controller_mode(CTRL_MODE_VELOCITY_CONTROL, INPUT_MODE_PASSTHROUGH);
+            odrive.set_input_vel(velocity_command * ECVT_DIR, 0.0f);
+        }
+
+        /* Update telemetry buffer with current values */
+        uint64_t time_us = esp_timer_get_time();
+        Telemetry::back_buffer->time_ms = (float) time_us / 1e3;
+
+        Telemetry::back_buffer->engine_rpm = primary_rpm;
+        Telemetry::back_buffer->secondary_rpm = secondary_rpm; 
+
+        Telemetry::back_buffer->filtered_engine_rpm = filtered_primary_rpm;
+        Telemetry::back_buffer->filtered_secondary_rpm = filtered_secondary_rpm;
+
+        Telemetry::back_buffer->target_rpm = ECVT_TARGET_RPM;
+        Telemetry::back_buffer->engine_rpm_error = engine_rpm_error; 
+
+        Telemetry::back_buffer->velocity_command = velocity_command; 
+        
+        Telemetry::back_buffer->ecvt_velocity = odrive.get_vel() * ECVT_DIR;
+        Telemetry::back_buffer->ecvt_pos = odrive.get_pos() * ECVT_DIR;
+        Telemetry::back_buffer->ecvt_iq = odrive.get_iq();
+
+        Telemetry::back_buffer->inbound_limit_switch = get_inbound_limit(); 
+        Telemetry::back_buffer->outbound_limit_switch = get_outbound_limit(); 
+        Telemetry::back_buffer->engage_limit_switch = get_engage_limit(); 
+        
+        Telemetry::back_buffer = Telemetry::front_buffer.exchange(Telemetry::back_buffer);
+
+        control_cycle_count++;
+    }
+}
+
 bool ECVTController::get_outbound_limit() {
     return !digitalRead(ECVT_LIMIT_SWITCH_OUTBOUND_PIN);
 }
@@ -227,4 +238,15 @@ void IRAM_ATTR ECVTController::inbound_isr(void* p) {
     if (instance) {
         instance->odrive.set_input_vel(0.0f, 0.0f);
     }
+}
+
+void ECVTController::timerCallback(void* arg)
+{
+    ECVTController* controller = (ECVTController*)arg;
+    vTaskNotifyGiveFromISR(controller->taskHandle, NULL);
+}
+
+void ECVTController::taskWrapper(void* pvParameters)
+{
+    ((ECVTController*)pvParameters)->control_loop();
 }
