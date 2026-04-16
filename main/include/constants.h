e2f62a8a4d22b8999d7159a3c1cf92957e5acd9a
sarah darzacq <sarahdarzacq22@gmail.com>
2026-04-15 20:14:40 -0700

    Organize constants file

--- main/include/constants.h
+++ main/include/constants.h
@@ -4,92 +4,52 @@
 #include <cstdint>
 #include <macros.h>
 #include "driver/gpio.h"
-/* Units */
-constexpr float SECONDS_PER_MINUTE = 60.0f; /* s / min */
-constexpr float MS_PER_SECOND = 1.0e3;     /* ms / s */ 
-constexpr float US_PER_SECOND = 1.0e6;     /* us / s */
-constexpr float SECONDS_PER_MS = 1.0e-3;   /* s / ms */
-constexpr float SECONDS_PER_US = 1.0e-6;   /* s / us */
 
-constexpr float MM_PER_INCH = 25.4f;              /* mm / inch */
-constexpr float INCHES_PER_MM = 1.0f / MM_PER_INCH; /* inch / mm */
-
-constexpr float FEET_PER_MILE = 5280.0f; /* feet / mile */
-constexpr float INCH_PER_FEET = 12.0f;   /* inch / feet */
-
-// Powertrain
-constexpr uint32_t ENGINE_SAMPLE_WINDOW = 4;
-constexpr uint32_t GEAR_SAMPLE_WINDOW = 10;
-
-constexpr float ENGINE_COUNTS_PER_ROT = 24; // count / rot
-constexpr float GEAR_COUNTS_PER_ROT = 6;    // count / rot
-
-constexpr float GEAR_TO_WHEEL_RATIO = 58.0f / 19.0f;                
-constexpr float GEAR_TO_SECONDARY_RATIO = 17.0f / 50.0f;
-
-constexpr float VELOCITY_LIMIT = 30.0f;
-constexpr float ECVT_HOME_SPEED = 4.0f;
-constexpr int ECVT_DIR = 1.0;
-
-constexpr uint32_t CONTROL_FUNCTION_INTERVAL_MS = 10;
-
-// CAN Values 
+/* CAN Values */
 constexpr uint32_t CAN_BITRATE = 250000;
 
 constexpr uint8_t ECVT_ODRIVE_NODE_ID = 1; 
 constexpr uint8_t CENTERLOCK_ODRIVE_NODE_ID = 3; 
 
-// Electronics Pins 
-constexpr uint32_t ENGINE_GEARTOOTH_SENSOR_PIN = 16;
-constexpr uint32_t GEARBOX_GEARTOOTH_SENSOR_PIN = 17;
-
-constexpr gpio_num_t BRAKE_POT_PIN = GPIO_NUM_15;
-constexpr gpio_num_t THROT_POT_PIN = GPIO_NUM_7;
-
-constexpr uint32_t SR_SER_IN_PIN = 21;  // serin 
-constexpr uint32_t SR_SHIFT_REG_CLK_PIN = 45; // srck
-constexpr uint32_t SR_REG_CLK_PIN = 46; // rck
-
-constexpr gpio_num_t ECVT_LIMIT_SWITCH_INBOUND_PIN = GPIO_NUM_12; 
-constexpr gpio_num_t ECVT_LIMIT_SWITCH_OUTBOUND_PIN = GPIO_NUM_10; 
-constexpr gpio_num_t ECVT_LIMIT_SWITCH_ENGAGE_PIN = GPIO_NUM_11; /* Not used */
-
-constexpr gpio_num_t CENTERLOCK_LIMIT_SWITCH_INBOUND_PIN = GPIO_NUM_18;
-constexpr gpio_num_t CENTERLOCK_LIMIT_SWITCH_OUTBOUND_PIN = GPIO_NUM_8;
-constexpr gpio_num_t CENTERLOCK_LED_PIN = GPIO_NUM_2;
-// constexpr gpio_num_t CENTERLOCK_SWITCH_1_PIN = GPIO_NUM_13; 
-// constexpr gpio_num_t CENTERLOCK_SWITCH_2_PIN = GPIO_NUM_6; 
-constexpr gpio_num_t CENTERLOCK_GTS_PIN = GPIO_NUM_9; 
-
-constexpr gpio_num_t CAN_TX_PIN = GPIO_NUM_5;
-constexpr gpio_num_t CAN_RX_PIN = GPIO_NUM_4; 
-
-constexpr gpio_num_t BUTTON_4_PIN = GPIO_NUM_2; 
-constexpr gpio_num_t BUTTON_3_PIN = GPIO_NUM_1; 
-constexpr gpio_num_t BUTTON_2_PIN = GPIO_NUM_44; 
-constexpr gpio_num_t BUTTON_1_PIN = GPIO_NUM_43; 
-
-constexpr gpio_num_t CENTERLOCK_SWITCH_1_PIN = BUTTON_1_PIN; 
-constexpr gpio_num_t CENTERLOCK_SWITCH_2_PIN = BUTTON_2_PIN; 
-
-constexpr gpio_num_t EXTRA_IO_2_PIN = GPIO_NUM_45; 
-constexpr gpio_num_t EXTRA_IO_1_PIN = GPIO_NUM_46; 
-constexpr gpio_num_t EXTRA_GTS_PIN = GPIO_NUM_3; 
-
+/* Odrive Values */
+constexpr float VELOCITY_LIMIT = 30.0f;
 constexpr float CENTERLOCK_ODRIVE_VEL_LIMIT = 30.0; 
-constexpr float CENTERLOCK_ODRIVE_CURRENT_LIMIT = 3.0;
+constexpr float CENTERLOCK_ODRIVE_CURRENT_LIMIT = 3.0; 
+
+/* Electronics */
+constexpr uint32_t CONTROL_FUNCTION_INTERVAL_MS = 10;
+
+constexpr float ECVT_HOME_SPEED = 4.0f;
+constexpr int ECVT_DIR = 1.0;
 
 constexpr float CENTERLOCK_HOME_VEL = 2; 
 constexpr float CENTERLOCK_VEL = 20; 
 constexpr float CENTERLOCK_DIR = 1; 
 
+/* ECVT Controller Values */
+constexpr float ACTUATOR_KP = 0.01f;   
+constexpr float ACTUATOR_KI = 0.000f;   
+constexpr float ACTUATOR_KD = 0.004f;
+
+constexpr float ECVT_TARGET_RPM = 3000.0f;
+constexpr float ACTUATOR_INBOUND_THRESHOLD = 8.0f;
+
+/* Powertrain */
+constexpr uint32_t ENGINE_SAMPLE_WINDOW = 4;
+constexpr uint32_t GEAR_SAMPLE_WINDOW = 10;
+
+constexpr float ENGINE_COUNTS_PER_ROT = 24; // count / rot
+constexpr float GEAR_COUNTS_PER_ROT = 50;    // count / rot
+
+constexpr float GEAR_TO_WHEEL_RATIO = 58.0f / 19.0f;                
+constexpr float GEAR_TO_SECONDARY_RATIO = 17.0f / 50.0f;
+
 /* DAQ Pinouts */
 constexpr int DAQ_LED_1_PIN = 21; 
-
 constexpr int DAQ_BUTTON_A_PIN = 5;
 constexpr int DAQ_BUTTON_B_PIN = 4;
 
-
+/* Filters */
 constexpr uint32_t ENGINE_RPM_MEDIAN_FILTER_WINDOW = 3;
 
 constexpr float ENGINE_RPM_ROTATION_FILTER_B[] = {
@@ -127,12 +87,52 @@ constexpr float THROTTLE_FILTER_A[] = {1., -0.88161859};
 constexpr size_t THROTTLE_FILTER_M = COUNT_OF(THROTTLE_FILTER_B);
 constexpr size_t THROTTLE_FILTER_N = COUNT_OF(THROTTLE_FILTER_A);
 
-constexpr float ACTUATOR_KP = 0.01f;   
-constexpr float ACTUATOR_KI = 0.000f;   
-constexpr float ACTUATOR_KD = 0.004f;
+/* Electronics Pins */
+constexpr uint32_t ENGINE_GEARTOOTH_SENSOR_PIN = 16;
+constexpr uint32_t GEARBOX_GEARTOOTH_SENSOR_PIN = 17;
 
-constexpr float ECVT_TARGET_RPM = 3000.0f;
+constexpr gpio_num_t BRAKE_POT_PIN = GPIO_NUM_15;
+constexpr gpio_num_t THROT_POT_PIN = GPIO_NUM_7;
 
-constexpr float ACTUATOR_INBOUND_THRESHOLD = 8.0f;
+constexpr uint32_t SR_SER_IN_PIN = 21;  // serin 
+constexpr uint32_t SR_SHIFT_REG_CLK_PIN = 45; // srck
+constexpr uint32_t SR_REG_CLK_PIN = 46; // rck
+
+constexpr gpio_num_t ECVT_LIMIT_SWITCH_INBOUND_PIN = GPIO_NUM_12; 
+constexpr gpio_num_t ECVT_LIMIT_SWITCH_OUTBOUND_PIN = GPIO_NUM_10; 
+constexpr gpio_num_t ECVT_LIMIT_SWITCH_ENGAGE_PIN = GPIO_NUM_11; /* Not used */
+
+constexpr gpio_num_t CENTERLOCK_LIMIT_SWITCH_INBOUND_PIN = GPIO_NUM_18;
+constexpr gpio_num_t CENTERLOCK_LIMIT_SWITCH_OUTBOUND_PIN = GPIO_NUM_8;
+constexpr gpio_num_t CENTERLOCK_LED_PIN = GPIO_NUM_2;
+constexpr gpio_num_t CENTERLOCK_GTS_PIN = GPIO_NUM_9; 
+
+constexpr gpio_num_t CAN_TX_PIN = GPIO_NUM_5;
+constexpr gpio_num_t CAN_RX_PIN = GPIO_NUM_4; 
+
+constexpr gpio_num_t BUTTON_4_PIN = GPIO_NUM_2; 
+constexpr gpio_num_t BUTTON_3_PIN = GPIO_NUM_1; 
+constexpr gpio_num_t BUTTON_2_PIN = GPIO_NUM_44; 
+constexpr gpio_num_t BUTTON_1_PIN = GPIO_NUM_43; 
+
+constexpr gpio_num_t CENTERLOCK_SWITCH_1_PIN = BUTTON_1_PIN; 
+constexpr gpio_num_t CENTERLOCK_SWITCH_2_PIN = BUTTON_2_PIN; 
+
+constexpr gpio_num_t EXTRA_IO_2_PIN = GPIO_NUM_45; 
+constexpr gpio_num_t EXTRA_IO_1_PIN = GPIO_NUM_46; 
+constexpr gpio_num_t EXTRA_GTS_PIN = GPIO_NUM_3;
+
+/* Units */
+constexpr float SECONDS_PER_MINUTE = 60.0f; /* s / min */
+constexpr float MS_PER_SECOND = 1.0e3;     /* ms / s */ 
+constexpr float US_PER_SECOND = 1.0e6;     /* us / s */
+constexpr float SECONDS_PER_MS = 1.0e-3;   /* s / ms */
+constexpr float SECONDS_PER_US = 1.0e-6;   /* s / us */
+
+constexpr float MM_PER_INCH = 25.4f;              /* mm / inch */
+constexpr float INCHES_PER_MM = 1.0f / MM_PER_INCH; /* inch / mm */
+
+constexpr float FEET_PER_MILE = 5280.0f; /* feet / mile */
+constexpr float INCH_PER_FEET = 12.0f;   /* inch / feet */
 
 #endif // CONSTANTS_H
