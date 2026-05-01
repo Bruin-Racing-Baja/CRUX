241e714716fdee5e614278324b9430ae5724967a
Arnav Jain <arnavj214@gmail.com>
2026-05-01 15:07:37 -0700

    add lora to daq logging

--- scripts/logger_daq_only.py
+++ scripts/logger_daq_only.py
@@ -5,11 +5,16 @@ import zlib
 import csv
 import os
 from datetime import datetime
-
-SERIAL_PORT      = 'daq_esp'
+import threading
+import queue
+SERIAL_PORT      = '/dev/daq_esp'
 BAUD_RATE        = 115200
 TIMEOUT_THRESHOLD = 2.0
 RECONNECT_DELAY  = 1.0
+LORA_PORT = '/dev/lora_esp'
+LORA_BAUD = 57600
+LORA_STRUCT_FORMAT = '<i i'            
+
 
 SYNC_1 = 0xFA
 SYNC_2 = 0xCE
@@ -22,6 +27,32 @@ DAQ_FIELDS = ["time_ms", "shock_rl_mm", "shock_rr_mm", "shock_rl_raw", "shock_rr
 DAQ_FMT    = f'<{len(DAQ_FIELDS)}f'
 DAQ_SIZE   = struct.calcsize(DAQ_FMT)
 
+
+def lora_handler():
+    lora_ser = None
+    while True:
+        if lora_ser is None or not lora_ser.is_open:
+            try:
+                # We use a short timeout so we don't hang the thread too long
+                lora_ser = serial.Serial(LORA_PORT, LORA_BAUD, timeout=0.05)
+                lora_ser.dtr = lora_ser.rts = False
+            except Exception:
+                lora_ser = None
+                time.sleep(2) # Don't spam the CPU if the device is missing
+                continue
+
+        try:
+            lat, lon = lora_queue.get(timeout=1.0)
+            
+            try:
+                packet = struct.pack(LORA_STRUCT_FORMAT, int(lat * 1e7), int(lon * 1e7))
+                lora_ser.write(packet)
+                lora_ser.flush()
+            except Exception:
+                lora_ser.close()
+                lora_ser = None
+        except queue.Empty:
+            continue
 
 def read_packet(ser):
     if ser.read(1) != bytes([SYNC_1]):
@@ -58,7 +89,12 @@ def main():
     print(f"--- DAQ LOGGER ---")
     print(f"Port: {SERIAL_PORT} @ {BAUD_RATE}")
     ser = None
-
+    lora_ser = None
+    if "lat" in DAQ_FIELDS and "lon" in DAQ_FIELDS:
+        LORA_LAT_INDEX = DAQ_FIELDS.index("lat")
+        LORA_LONG_INDEX = DAQ_FIELDS.index("lon")
+    lora_thread = threading.Thread(target=lora_handler, daemon=True)
+    lora_thread.start()
     while True:
         try:
             if ser is None or not ser.is_open:
@@ -75,6 +111,17 @@ def main():
                 time.sleep(0.1)
                 ser.reset_input_buffer()
                 print("Connected!")
+
+            if lora_ser is None or not lora_ser.is_open:
+                # print(f"Connecting to {CONTROLS_PORT}...")
+                # ser = serial.Serial(CONTROLS_PORT, CONTROLS_BAUD, timeout=0.1)
+                # ser.dtr = False
+                # ser.rts = False
+                # ser.reset_input_buffer()
+                lora_ser = serial.Serial(LORA_PORT, LORA_BAUD, timeout=0.1)
+                lora_ser.dtr = False
+                lora_ser.rts = False
+                lora_ser.reset_input_buffer()
 
             os.makedirs("logs/daq", exist_ok=True)
             log_filename = f"logs/daq/log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
@@ -105,6 +152,14 @@ def main():
                     packets += 1
 
                     if time.time() - last_print > 1.0:
+                        if "lat" in DAQ_FIELDS and "lon" in DAQ_FIELDS:
+                            try:
+                                if not lora_queue.empty():
+                                    lora_queue.get_nowait()
+                                
+                                lora_queue.put_nowait((values[LORA_LAT_INDEX], values[LORA_LONG_INDEX]))
+                            except Exception:
+                                pass
                         print(f"{packets} pkts/s | seq={seq_num} | crc_errors={crc_errors}")
                         packets   = 0
                         last_print = time.time()
